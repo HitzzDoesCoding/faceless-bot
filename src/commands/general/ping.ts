26066d4b889a7847d0eb129394cbc223e217b003
Hitzz <callummp75@gmail.com>
2023-04-13 00:46:11 +0100

    init

---
+++ src/commands/general/ping.ts
@@ -0,0 +1,37 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, inlineCode, Message } from 'discord.js';
+import { CommandClass } from '../../structures/command.js';
+
+export default new CommandClass({
+    data: new SlashCommandBuilder()
+        .setName('radio')
+        .setDescription('Generates Radio Frequency'),
+    opt: {
+        userPermissions: ['SendMessages'],
+        botPermissions: ['SendMessages'],
+        category: 'General',
+        cooldown: 5,
+        visible: true,
+        guildOnly: false,
+    },
+    async execute(interaction: ChatInputCommandInteraction<'cached'>) {
+        const oldMsg = await interaction.channel.messages.fetch({ limit: 1 }).then(messages => messages.first()) as Message;
+        let oldFrequency = '';
+        if (oldMsg?.author.bot) {
+            oldFrequency = oldMsg.content.match(/\d+\.\d+/)?.[0] || '';
+            await oldMsg.delete();
+        }
+
+        const msg = await interaction.reply({
+            content: 'Generating...\nPrevious frequency: ' + (oldFrequency ? inlineCode(`${oldFrequency}`) : 'N/A'),
+            fetchReply: true
+        });
+
+        setTimeout(() => {
+            const Frequency = (Math.random() * 98 + 1).toFixed(2);
+            interaction.editReply({
+                content: `New Frequency Is ${inlineCode(`${Frequency}`)}.\nPrevious frequency: ${oldFrequency ? inlineCode(`${oldFrequency}`) : 'N/A'}`,
+            });
+        }, 3000);
+        console.log(msg)
+    },
+});
