7fe579dabb3398124cd5c2ae9c6e738107a35cce
Hitzz <callummp75@gmail.com>
2023-04-13 02:11:36 +0100

    Update: QoL update to frequency spoiler added

--- src/commands/general/radio.ts
+++ src/commands/general/radio.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, inlineCode, Message } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, inlineCode, Message, spoiler } from 'discord.js';
 import { CommandClass } from '../../structures/command.js';
 
 export default new CommandClass({
@@ -29,7 +29,7 @@ export default new CommandClass({
         setTimeout(() => {
             const Frequency = (Math.random() * 98 + 1).toFixed(2);
             interaction.editReply({
-                content: `New Frequency Is ${inlineCode(`${Frequency}`)}.\nPrevious frequency: ${oldFrequency ? inlineCode(`${oldFrequency}`) : 'N/A'}`,
+                content: `New Frequency Is ${spoiler(`${Frequency}`)}.\nPrevious frequency: ${oldFrequency ? spoiler(`${oldFrequency}`) : 'N/A'}`,
             });
         }, 3000);
         console.log(msg)
